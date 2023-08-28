a5e59f35c904ab329c1549e324ade42f036fa4d5
youta19851124 <youta19851124@gmail.com>
2023-08-28 14:17:26 +0900

    ユーザー情報編集機能の実装、ビュー調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,26 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
+    @introduction = user.introduction
     @catches = user.catches
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      flash.now[:error] = "更新に失敗しました。"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :introduction)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true
+
   has_many :catches
   has_many :comments
 end
